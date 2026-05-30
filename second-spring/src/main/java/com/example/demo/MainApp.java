d99d2e1a33552ef94e4001a710db827459104fe5
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-05-30 23:33:47 +0530

    second spring

--- second-spring/src/main/java/com/example/demo/MainApp.java
+++ second-spring/src/main/java/com/example/demo/MainApp.java
@@ -2,24 +2,25 @@ package com.example.demo;
 
 import com.example.loose.UserService;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
     public static void main(String[] args) {
 
         ApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationBeanContext.xml");
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
         GreetingService greetingService =
                 context.getBean("myBean", GreetingService.class);
         greetingService.sayHello();
 
-        UserService userServiceSMS =
-                context.getBean("userServiceSMS", UserService.class);
-        userServiceSMS.notifyUser("Whats up!");
-
-        UserService userServiceEmail =
-                context.getBean("userServiceEmail", UserService.class);
-        userServiceEmail.notifyUser("Whats up!");
+//        UserService userServiceSMS =
+//                context.getBean("userServiceSMS", UserService.class);
+//        userServiceSMS.notifyUser("Whats up!");
+//
+//        UserService userServiceEmail =
+//                context.getBean("userServiceEmail", UserService.class);
+//        userServiceEmail.notifyUser("Whats up!");
     }
 }
