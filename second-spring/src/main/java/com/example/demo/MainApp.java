f4aeca6a9c26449568583413c436911e6105abce
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-05-30 23:19:26 +0530

    second spring

---
+++ second-spring/src/main/java/com/example/demo/MainApp.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import com.example.loose.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationBeanContext.xml");
+
+        GreetingService greetingService =
+                context.getBean("myBean", GreetingService.class);
+        greetingService.sayHello();
+
+        UserService userServiceSMS =
+                context.getBean("userServiceSMS", UserService.class);
+        userServiceSMS.notifyUser("Whats up!");
+
+        UserService userServiceEmail =
+                context.getBean("userServiceEmail", UserService.class);
+        userServiceEmail.notifyUser("Whats up!");
+    }
+}
---
+++ second-spring/src/main/java/com/example/loose/UserService.java
@@ -0,0 +1,16 @@
+package com.example.loose;
+
+public class UserService {
+    NotificationService notificationService;
+
+    public UserService(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+    public void notifyUser(String message){
+        notificationService.send("Notification hello");
+    }
+
+    public void setNotificationService(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+}
