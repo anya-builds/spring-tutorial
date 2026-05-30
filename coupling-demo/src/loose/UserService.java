bf8a3deb816d8c399b2e70cc4d47f63f31117768
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-05-30 06:22:44 +0530

    coupling

---
+++ coupling-demo/src/loose/UserService.java
@@ -0,0 +1,12 @@
+package loose;
+
+public class UserService {
+    NotificationService notificationService;
+
+    public UserService(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+    public void notifyUser(String message){
+        notificationService.send("Notification hello");
+    }
+}
