53fd83f79c62f69382351e74846f024673aed46a
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-06-02 01:15:12 +0530

    JDBC

--- jdbc-demo/src/main/java/JDBCDemo.java
+++ jdbc-demo/src/main/java/JDBCDemo.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JDBCDemo {
 
@@ -12,7 +9,9 @@ public class JDBCDemo {
     public static void main(String[] args) {
         try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
             System.out.println("Connected to the database!");
-            insertStudent(conn, "Alice", "alice@gmil.com");
+//            insertStudent(conn, "Alice", "alice@gmil.com");
+            updateStudent(conn,1,"Bob","alice@gmail.com");
+            selectStudent(conn);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -24,7 +23,37 @@ public class JDBCDemo {
         try (Statement stmt = conn.createStatement()) {
             int rows = stmt.executeUpdate(sql);
             System.out.println("INSERTED: " + rows);
+            selectStudent(conn);
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void selectStudent(Connection conn){
+        String sql="SELECT * FROM student";
+        try (Statement stmt=conn.createStatement()){
+           ResultSet resultSet=stmt.executeQuery(sql);
+            System.out.println("Student List: ");
+            while (resultSet.next()){
+                int id= resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email= resultSet.getString("email");
+                System.out.println(id + " : " + name + " : "+ email);
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private  static void updateStudent(Connection conn,int id,String name,String email){
+        String sql = "UPDATE student SET name = '" + name + "', email = '" + email + "' WHERE id = " + id;
+        // UPDATE student set name ='Alice',email='email@gmail.com'
+        // where id=10;
+        try (Statement stmt = conn.createStatement()){
+            int rows=stmt.executeUpdate(sql);
+            System.out.println("UPDATED: "+rows);
+
+        }catch (SQLException e){
             e.printStackTrace();
         }
     }
