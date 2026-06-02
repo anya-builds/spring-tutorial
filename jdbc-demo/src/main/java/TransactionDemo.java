3babfc28fb0ced07c857284e5082834541014590
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-06-02 21:05:06 +0530

    Transaction

--- jdbc-demo/src/main/java/TransactionDemo.java
+++ jdbc-demo/src/main/java/TransactionDemo.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class TransactionDemo {
@@ -9,10 +10,27 @@ public class TransactionDemo {
  public static void main(String[] args) {
      try (Connection conn= DriverManager.getConnection(URL,USER,PASSWORD)){
          System.out.println("Connected to the database");
+
+         //order , orderItems
+         //insert into order
+         int orderId= insertOrder(conn,101,"Alice01",2000.0);
+         //insert into order item
+         insertOrderItem(conn, orderId,"Laptop01",1,2000.0);
      }catch (SQLException e){
          e.printStackTrace();
      }
 
 
  }
+
+    private static int insertOrder(Connection conn, int customerId, String customerName, double price) {
+     String sql="INSERT INTO orders (user_id,customer_name, total_amount) values (?,?,?)";
+     try (PreparedStatement pstmt=conn.prepareStatement(sql)){
+            pstmt.setInt(1,customerId);
+            pstmt.setString(2,customerName);
+            pstmt.setDouble(3,price);
+     }catch (SQLException e){
+         e.printStackTrace();
+     }
+    }
 }
