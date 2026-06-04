db1827e37dc09d44e2ebf783ff3bdaecd67d1d05
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-06-04 07:47:04 +0530

    JPA entity manager

--- jpademo/src/main/java/com/example/jpademo/UserService.java
+++ jpademo/src/main/java/com/example/jpademo/UserService.java
@@ -2,11 +2,20 @@ package com.example.jpademo;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+    public UserClassSpringBoot createUser(UserClassSpringBoot user){
+        return userRepository.save(user);
+    }
+
+    public List<UserClassSpringBoot> getAAllUsers() {
+        return userRepository.findAll();
     }
 }
